4140a6d7a3964252398049748c89ee90f88e6cb9
Paola Noriega <paonordele@gmail.com>
2022-04-21 17:33:23 -0500

    Exercise TempConversion

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,13 +1,32 @@
-const ftoc = function() {
+const ftoc = function(number) {
+  var cels = 0;
+  var rounded = 0;
+
+  cels = ((number-32) * (5/9));
+  rounded = Math.round(cels * 10)/10;
+  
+  console.log(rounded);
+  return rounded;
 
 };
 
-const ctof = function() {
+const ctof = function(number) {
+  var farh = 0;
+  var rounded = 0;
+
+  farh = ((number * (9/5)) + 32);
+  rounded = Math.round(farh * 10)/10;
+
+  console.log(rounded);
+  return rounded;
+
 
 };
+
+
 
 // Do not edit below this line
 module.exports = {
-  ftoc,
-  ctof
+ftoc,
+ctof
 };
