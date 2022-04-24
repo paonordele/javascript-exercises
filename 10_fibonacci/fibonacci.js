945b52938982b15f0d26a7b9d2a22d32e54ba7b1
Paola Noriega <paonordele@gmail.com>
2022-04-23 21:38:13 -0500

    Fibonacci 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,6 +1,30 @@
-const fibonacci = function() {
+const fibonacci = function(number1) 
+{
+    var fibonacci = [];
+    var IntNumber1 = Math.sign(number1);
+
+    fibonacci[0] = 0;
+    fibonacci[1] = 1;
+    
+    
+    if(IntNumber1 == -1)
+    {
+        return "OOPS";
+        //break;
+    }
+    
+    for (var i = 2; i <=number1; i++) 
+    {
+        fibonacci[i] = fibonacci[i - 2] + fibonacci[i - 1];
+    }
+
+
+    console.log(fibonacci);
+    console.log(fibonacci[number1]);
+    return fibonacci[number1];
 
 };
+
 
 // Do not edit below this line
 module.exports = fibonacci;
