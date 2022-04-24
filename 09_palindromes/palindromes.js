736fb5fc1e0271bf9315643f47f0d5253151e59f
Paola Noriega <paonordele@gmail.com>
2022-04-23 20:52:06 -0500

    Palindromes 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,6 +1,23 @@
-const palindromes = function () {
+const palindromes = function (string1) {
+    var palindrome = [];
+    var len = (string1.length)-1;
+    var IsPalindrome = true;
 
+    for(var i=len; i>=0; i--){
+        var lenpal = (len-i)
+        palindrome[lenpal] = string1[i];
+    }
+    for(var j=len; j>=0; j--){
+        var lenpal = (len-i)
+        if (string1[i] != palindrome[lenpal]){
+            IsPalindrome = false;
+        }    
+    }
+        
+    return IsPalindrome;
+       
 };
+
 
 // Do not edit below this line
 module.exports = palindromes;
